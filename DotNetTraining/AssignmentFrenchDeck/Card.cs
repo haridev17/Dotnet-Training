a93072e6a185c842bc92a1325053227fa6327bf3
haridev17 <haridev.b@edstem.com>
2023-01-12 22:51:08 +0530

    FrenchDeck

---
+++ DotNetTraining/AssignmentFrenchDeck/Card.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApplication1
+{
+    class Card
+    {
+        protected Suits suit;
+        protected string cardvalue;
+        public Card(Suits suit2, string cardvalue2)
+        {
+            suit = suit2;
+            cardvalue = cardvalue2;
+        }
+        public override string ToString()
+        {
+            return ($"{cardvalue} of {suit}");
+        }
+    }
+}
---
+++ DotNetTraining/AssignmentFrenchDeck/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck mydeck = new Deck();
+            Console.WriteLine("printing deck");
+            foreach (Card c in mydeck.Cards)
+            {   
+                Console.WriteLine(c);
+            }
+   
+        }
+        static Random r = new Random();
+        static public void Shuffle(int[] deck)
+        {
+            for (int n = deck.Length - 1; n > 0; --n)
+            {
+                int k = r.Next(n + 1);
+                int temp = deck[n];
+                deck[n] = deck[k];
+                deck[k] = temp;
+                
+            }
+            Console.WriteLine(deck);
+
+        }
+
+    }
+}
