eaa809b8bd56469abe0ec31accddefc1c815e2c7
haridev17 <haridev.b@edstem.com>
2023-01-12 22:46:03 +0530

    shopping cart

---
+++ DotNetTraining/AssignmentDay3Shopping/Order.cs
@@ -0,0 +1,11 @@
+﻿class Order
+{
+    private double _tax = 0.01;//10 percent
+    public double GetTotalPrice(double productPrice)
+    {
+        double totalPrice = productPrice + productPrice * _tax;
+        return totalPrice;
+    }
+}
+
+public record ShoppingCartEntry(string Name, int Price, int Quantity);
---
+++ DotNetTraining/AssignmentDay3Shopping/Program.cs
@@ -0,0 +1,61 @@
+﻿//shopping cart
+int option = 0;
+
+Console.WriteLine("Enter item to purchase");
+Console.WriteLine("1. laptop: Press 1");
+Console.WriteLine("2. bag: Press 2");
+Console.WriteLine("3. mobile: Press 3");
+Console.WriteLine("4. Quit: Press 4");
+option = Convert.ToInt32(Console.ReadLine());
+
+ShoppingCartList shoppingCartList = new ShoppingCartList();
+
+switch (option)
+{
+    case 1:
+
+        ShoppingCartEntry item1 = new ShoppingCartEntry("laptop", 5000, 1);
+        shoppingCartList.AddToCart(item1);
+        break;
+
+    case 2:
+        ShoppingCartEntry item2 = new ShoppingCartEntry("bag", 1500, 1);
+        shoppingCartList.AddToCart(item2);
+        break;
+
+    case 3:
+        ShoppingCartEntry item3 = new ShoppingCartEntry("mobile", 400, 1);
+        shoppingCartList.AddToCart(item3);
+        break;
+
+    case 4:
+        break;
+
+    default:
+        Console.WriteLine("invalid choice");
+        break;
+
+}
+
+
+
+void TotalPrice()
+{
+    Order order = new Order();
+    double totalPrice = shoppingCartList.TotalPriceCalculation();
+    Console.WriteLine(order.GetTotalPrice(totalPrice));
+}
+try
+{
+    shoppingCartList.PrintCart();
+    TotalPrice();
+}
+catch (Exception e)
+{
+
+    Console.WriteLine($"Something went wrong{e.Message}");
+}
+finally
+{
+    Console.WriteLine("Shop with us again");
+}
