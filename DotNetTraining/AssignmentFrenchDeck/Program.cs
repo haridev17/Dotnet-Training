edad7051b7d933df172c64fc38d63d3d81e30fa1
HARIDEV <107237329+haridev17@users.noreply.github.com>
2023-01-13 07:51:38 +0530

    Update Program.cs

--- DotNetTraining/AssignmentFrenchDeck/Program.cs
+++ DotNetTraining/AssignmentFrenchDeck/Program.cs
@@ -12,20 +12,20 @@
             }
    
         }
-        static Random r = new Random();
-        static public void Shuffle(int[] deck)
-        {
-            for (int n = deck.Length - 1; n > 0; --n)
-            {
-                int k = r.Next(n + 1);
-                int temp = deck[n];
-                deck[n] = deck[k];
-                deck[k] = temp;
+        //static Random r = new Random();
+        //static public void Shuffle(int[] deck)
+       // {
+           // for (int n = deck.Length - 1; n > 0; --n)
+           // {
+               // int k = r.Next(n + 1);
+               // int temp = deck[n];
+              //  deck[n] = deck[k];
+               // deck[k] = temp;
                 
-            }
-            Console.WriteLine(deck);
+           // }
+           // Console.WriteLine(deck);
 
-        }
+       // }
 
     }
 }
