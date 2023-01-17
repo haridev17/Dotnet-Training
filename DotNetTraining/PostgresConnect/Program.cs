000330b82fc54b327c51f476bfc757e52c44989d
haridev17 <haridev.b@edstem.com>
2023-01-17 08:16:34 +0530

    basic postgres connection creation and data read and write to postgres db

---
+++ DotNetTraining/PostgresConnect/Program.cs
@@ -0,0 +1,54 @@
+﻿using Npgsql;
+
+var cs = "Host=localhost;Username=postgres;Password=postgres;Database=dotnetconnect";
+
+using var con = new NpgsqlConnection(cs);
+con.Open();
+
+using var cmd = new NpgsqlCommand();
+cmd.Connection = con;
+
+cmd.CommandText = "DROP TABLE IF EXISTS cars";
+cmd.ExecuteNonQuery();
+
+cmd.CommandText = @"CREATE TABLE cars(id SERIAL PRIMARY KEY,
+        name VARCHAR(255), price INT)";
+cmd.ExecuteNonQuery();
+
+cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Audi',52642)";
+cmd.ExecuteNonQuery();
+
+cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Mercedes',57127)";
+cmd.ExecuteNonQuery();
+
+cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Skoda',9000)";
+cmd.ExecuteNonQuery();
+
+cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Volvo',29000)";
+cmd.ExecuteNonQuery();
+
+cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Bentley',350000)";
+cmd.ExecuteNonQuery();
+
+cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Citroen',21000)";
+cmd.ExecuteNonQuery();
+
+cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Hummer',41400)";
+cmd.ExecuteNonQuery();
+
+cmd.CommandText = "INSERT INTO cars(name, price) VALUES('Volkswagen',21600)";
+cmd.ExecuteNonQuery();
+
+Console.WriteLine("Table cars created");
+string sql = "SELECT * FROM cars";
+using var cmdx = new NpgsqlCommand(sql, con);
+
+string sqls = "SELECT * FROM cars";
+
+using NpgsqlDataReader rdr = cmdx.ExecuteReader();
+
+while (rdr.Read())
+{
+    Console.WriteLine("{0} {1} {2}", rdr.GetInt32(0), rdr.GetString(1),
+            rdr.GetInt32(2));
+}
